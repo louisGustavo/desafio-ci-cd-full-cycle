c4ca04c5c02c4904de2a56be224743360760ba47
louisMascarenhas <luis.mascarenhas@kabum.com.br>
2023-05-10 20:13:53 -0300

    test: should sum two values and return the result

--- test/calc.spec.ts
+++ test/calc.spec.ts
@@ -1,6 +1,12 @@
 import Calc from "../src/Calc";
 
 test("should successfully instantiate Calc class", () => {
-    const calc = new Calc();
+    const calc = new Calc(1, 2);
     expect(calc).toBeInstanceOf(Calc);
+});
+
+test("should sum two values and return the result", () => {
+    const calc = new Calc(2, 3);
+    const sum = calc.sum();
+    expect(sum).toBe(5);
 });
