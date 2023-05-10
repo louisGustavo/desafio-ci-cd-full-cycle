570963ad60bff1bc28de93cb0a45cd87ff7e87c4
louisMascarenhas <luis.mascarenhas@kabum.com.br>
2023-05-10 20:18:18 -0300

    feat: should subtract two values and return the result

--- src/Calc.ts
+++ src/Calc.ts
@@ -1,13 +1,17 @@
 export default class Calc {
-    number1: number;
-    number2: number;
+    private readonly number1: number;
+    private readonly number2: number;
 
     constructor(number1: number, number2: number) {
         this.number1 = number1;
         this.number2 = number2;
     }
 
-    sum(): number {
+    public sum(): number {
         return this.number1 + this.number2;
+    }
+
+    public subtract(): number {
+        return this.number1 - this.number2;
     }
 }
