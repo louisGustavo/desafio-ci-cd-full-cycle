c1df1d7727251a4a26206a63e894c80c1858a7a3
louisMascarenhas <luis.mascarenhas@kabum.com.br>
2023-05-10 20:14:09 -0300

    feat: should sum two values and return the result

--- src/Calc.ts
+++ src/Calc.ts
@@ -1,1 +1,13 @@
-export default class Calc {}
+export default class Calc {
+    number1: number;
+    number2: number;
+
+    constructor(number1: number, number2: number) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    sum(): number {
+        return this.number1 + this.number2;
+    }
+}
